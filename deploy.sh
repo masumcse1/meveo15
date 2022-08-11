76c9bcd7fa9c4fc169746a74df1c8e8d76c8d890
masumcse1 <optinfra.masum@gmail.com>
2022-08-11 22:14:02 +0600

    final image test

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+MEVEO_DEPLOY_DIR="/opt/jboss/wildfly/standalone/deployments" 
+
+echo "Build meveo project"
+mvn clean package -DskipTests
+
+echo "Deploy meveo.ear in exploded mode"
+unzip meveo-ear/target/meveo.ear -d ${MEVEO_DEPLOY_DIR}/meveo.ear
+touch ${MEVEO_DEPLOY_DIR}/meveo.ear.dodeploy
+
+
